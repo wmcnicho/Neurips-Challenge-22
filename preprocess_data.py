fe6001b85fb606b58b9def2f575adf0c614ecd5e
huntermcnicholsmbp <hmcnich@gmail.com>
2022-09-10 12:19:47 -0400

    Preprocessing data tested and working, sample tensor file included to speed up workflow

--- preprocess_data.py
+++ preprocess_data.py
@@ -2,36 +2,45 @@ from unittest import result
 import pandas as pd
 from scipy import stats
 import torch
-import torch.nn.functional as F
 
 def createDataset(filename: str):
     lessons_df = pd.read_csv(filename)
     # This is pretty aggresive cleaning, maybe others Types are useful
     checkin_df = lessons_df[lessons_df['Type'] == 'Checkin']
     # Clean out all the columns we don't need
-    simple_df = checkin_df.iloc[:, [2, 5, 9]] # [3, ]
-    simple_df.loc[simple_df["IsCorrect"] == 0, "IsCorrect"] = -1
+    simple_df = checkin_df.iloc[:, [2, 5, 9]] # [S, 3]
+    simple_df.loc[simple_df["IsCorrect"] == 0, "IsCorrect"] = -1 # [S, 3]
     
     # A little dirty importing a module just for mode
-    num_max_questions = stats.mode(simple_df["UserId"]).count[0]
+    num_questions = stats.mode(simple_df["UserId"]).count[0] # Q
 
-    #C=num_constructs S=num_students
-    result = torch.tensor([]) # [1] -> [S, C+1]
+    #C=num_constructs S=num_students Q=num_questions
+    result = torch.tensor([]) # [1] -> [2, S, Q]
     for name, group in simple_df.groupby('UserId'):
-        print(group)
-        constructs = torch.tensor(group["ConstructId"].values.tolist()) # [C]
-        correct = torch.tensor(group["IsCorrect"].values.tolist()) # [1]
-        student = torch.tensor([constructs], [correct]) # [C +1]
-        
-        pad_needed = num_max_questions - student.shape[-1] # [p]
-        # This pads the bottom of the matrix with pad_needed rows
-        # Are we sure we need this?
-        #padded = F.pad(input=student, pad=(0, 0, 0, pad_needed)) # [C+p, 1+p]
-        #result_features = torch.stack(result_features, padded)
-    return result
+        # Extract info for each user
+        constructs = group["ConstructId"].values.tolist() # [C]
+        correct = group["IsCorrect"].values.tolist() # [C]
+
+        # Pad info
+        num_constructs = len(constructs)
+        pad_needed = num_questions - num_constructs # [P = Q - C]
+        constructs += [0] * pad_needed # [Q]
+        correct += [0] * pad_needed # [Q]
+
+        # Build tensor
+        student = torch.tensor([constructs, correct]) # [2, Q]
+        student = torch.unsqueeze(student, 0) # [1, 2, Q]
+
+        # Add to running result
+        result = torch.cat([result, student]) # [i, 2, Q]
+    return result # [S, 2, Q]
     
 def main():
-    tensor = createDataset('data/sample_data_lessons_small.csv')
+    #student_data = createDataset('data/sample_data_lessons_small.csv') # [S, 2, Q] Faster and better for debugging
+    student_data = createDataset('data/Task_3_dataset/checkins_lessons_checkouts_training.csv') # [S, 2, Q]
+    features = student_data[:, 0, :] # [S, Q]
+    labels = student_data[:, 1, :] # [S, Q]
 
 if __name__ == "__main__":
+    # loaded_file = torch.load('serialized_torch/student_data_tensor.pt') # This is also an option
     main()
